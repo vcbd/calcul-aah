a7cfac0096b321515bfe751d3415fa989bd5d6f1
Vincent Bradier <vincent@bradier.net>
2022-01-14 15:13:38 +0100

    feat: charts

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,10 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+  	'~/plugins/vue-lodash',
+    '~/plugins/vue-morris'
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
---
+++ plugins/vue-lodash.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import VueLodash from 'vue-lodash'
+
+// To be able to use this._ or Vue._ in the components without including lodash
+Vue.use(VueLodash)
+
